8cd4d22d24283cf32f9863d359d196f8e3ee7e12
monusbence <72020098+monusbence@users.noreply.github.com>
2023-01-26 21:32:07 +0100

    Add files via upload

---
+++ Házifeladat-2023.01.26/Házifeladat-2023.01.26/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Házifeladat_2023._01._26
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
